87926cd783e700582187b47b9c20e279db4f43d1
Laniez <adrien@mac.home>
2026-05-17 09:36:09 +0200

    fix(config): resolve vite source alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import { resolve } from 'node:path'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     tanstackStart({
       router: {
